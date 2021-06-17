cad60fd2dfa4794c396c157466d7b0d36636dd38
JungJaeLee-JJ <tjems8637@gmail.com>
2021-06-17 14:34:08 +0900

    cors 해결

--- kkuuk/settings.py
+++ kkuuk/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-ca%umw-rav(_b%64$)s64)2!*cm=uqzsjyft3%c52&zfzdlyb0
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000' ,'http://localhost:3000'] 
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'kkuukApp.apps.KkuukappConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'kkuuk.urls'
